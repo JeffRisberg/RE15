b6bfc3b9ba3c32b6a69c76f11ba6fcdd8b26bc54
JeffRisberg <jrisberg@aol.com>
2017-02-02 09:07:55 -0800

    added use of react-router

--- app/app.js
+++ app/app.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
-import charities from './reducers/charities';
-import donations from './reducers/donations';
+import charities from './reducers/charities'
+import donations from './reducers/donations'
 
-import NamedStyleResolution from './components/NamedStyleResolution';
-import ComposedStyleResolution from './components/ComposedStyleResolution';
-import CharityList from './components/CharityList';
-import DonationList from './components/DonationList';
+import AppRoot from './components/AppRoot'
+import Home from './components/Home'
+import NamedStyleResolution from './components/NamedStyleResolution'
+import ComposedStyleResolution from './components/ComposedStyleResolution'
+import CharityList from './components/CharityList'
+import DonationList from './components/DonationList'
 
 var initialContent = {
     charities: {idList: [], records: {}},
     donations: {idList: [], records: {}}
-};
+}
 
 const combinedReducers = combineReducers({
     charities,
     donations
-});
+})
 
 const store = createStore(
     combinedReducers,
     initialContent,
     applyMiddleware(thunkMiddleware)
-);
+)
 
 ReactDom.render(
     <Provider store={store}>
@@ -35,9 +38,14 @@ ReactDom.render(
             <NamedStyleResolution />
             <ComposedStyleResolution />
             <br/>
-            <br/>
-            <CharityList />
-            <br/>
-            <DonationList />
+            <Router history={hashHistory}>
+                <Route path="/" component={AppRoot}>
+                    <IndexRoute component={Home}/>
+                    <Route path="charities" component={CharityList}/>
+                    <Route path="donations" component={DonationList}/>
+                </Route>
+            </Router>
         </div>
-    </Provider>, document.getElementById('main'));
+    </Provider>,
+    document.getElementById('main')
+)
